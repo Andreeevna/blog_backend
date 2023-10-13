1d44bc23968aec9dfae7afad68e5172038455919
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-10-13 18:41:42 +0300

    created a connection to the database

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+mongoose
+	.connect(
+		'mongodb+srv://sandreeevna:Lana199881@cluster0.u02m9me.mongodb.net/?retryWrites=true&w=majority'
+	)
+	.then(() => {
+		console.log('db OK')
+	})
+	.catch(err => console.log('db err', err))
 
 const app = express()
 
