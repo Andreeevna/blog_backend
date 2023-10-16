605d0a425099cbf6a7bcaf8c686cf81845b82e2d
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-10-16 15:56:23 +0300

    added functionality for tags

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
 import multer from 'multer'
@@ -23,9 +24,6 @@ const app = express()
 
 const storage = multer.diskStorage({
 	destination: (_, __, cb) => {
-		// if (!fs.existsSync('uploads')) {
-		// 	fs.mkdirSync('uploads')
-		// }
 		cb(null, 'uploads')
 	},
 	filename: (_, file, cb) => {
@@ -36,6 +34,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.get('/', (req, res) => {
@@ -54,6 +53,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 
 app.get('/auth/me', checkAuth, getMe)
 
+app.get('/tags', PostController.getLastTags)
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
 app.post(
